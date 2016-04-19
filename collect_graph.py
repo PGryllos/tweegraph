7d4d1cbb5a5fe771df08ae8960bb7e51905cc3fb
prvl <gryllosprokopis@gmail.com>
2016-04-20 01:28:50 +0200

    Update collect_graph script

--- collect_graph.py
+++ collect_graph.py
@@ -1,40 +1,27 @@
+# script to collect a graph with 10000 twitter relations
 import json
 from time import sleep
 
 from tweegraph.traverser import TwitterGraphTraverser
-from pymongo import MongoClient
-
-
-def number_of_users(db_name, collection_name):
-
-    client = MongoClient()
-    collection = client[db_name][collection_name]
-
-    users = []
-
-    for user in collection.find():
-        users.append(user)
-    return len(users)
-
 
 if __name__ == "__main__":
 
     with open('credentials.json') as credentials_file:
         credentials = json.load(credentials_file)
 
-    starting_ids = [21447363,   # katy perry
-                    27260086,   # justin bieber
-                    813286,     # barack obama
-                    25365536,   # kim kardashian
-                    23375688,   # selena gomez
-                    50393960,   # bill gates
-                    31927467,]  # pitbull
-    db_name = 'twitter_dinata_5'
+    starting_ids = [21447363,    # katy perry
+                    27260086,    # justin bieber
+                    813286,      # barack obama
+                    25365536,    # kim kardashian
+                    23375688,    # selena gomez
+                    50393960,    # bill gates
+                    31927467,    # pitbull
+                    2292454922]  # tsipras_eu
 
     # create TwitterGraphTraverser instance
-    traverser = TwitterGraphTraverser(breadth=400,
-                                      graph_size=10000,
-                                      starting_ids=starting_ids,
+    traverser = TwitterGraphTraverser(breadth=600,
+                                      graph_size=30000,
+                                      starting_ids=starting_id,
                                       credentials=credentials)
     # start exploring graph
     traverser.start()
@@ -42,5 +29,4 @@ if __name__ == "__main__":
     while True:
         if traverser.size() > 0:
             traverser.exportData()
-            #print 'users in db: ', number_of_users(db_name, 'timelines')
             sleep(5)
