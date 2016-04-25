2187a402bc93b6ce5c9e5a38a5df4c5bf7636fcf
prvl <gryllosprokopis@gmail.com>
2016-04-25 02:40:09 +0200

    Create separate folder for non crawling scripts

---
+++ scripts/plot_graph.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from argparse import ArgumentParser
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='draw network')
+
+    parser.add_argument('input_file',
+                        metavar='input_file', type=str,
+                        help='csv file that describes edges of the graph')
+
+    args = parser.parse_args()
+    file_name = args.input_file
+
+    links = pd.read_csv(file_name, names=['follower', 'node'])
+
+    graph = nx.Graph()
+    # add nodes and edges
+    graph.add_edges_from(list(np.asarray(links.loc[1:, :])))
+
+    nx.draw(graph)
+    plt.show()
