70da10793508f7d52672c5d185fadb8410f0df2d
prvl <gryllosprokopis@gmail.com>
2016-05-14 18:50:14 +0300

    Add simple test cases for json handling methods

---
+++ tweegraph/tests/test_data.py
@@ -0,0 +1,38 @@
+import tweepy
+
+from data import get_unique_nodes_from_dict as un_nodes_dict
+from data import get_edges_from_dict as edges_dict
+
+
+test_dict = {
+            '1': {
+                    'followers': [2, 3, 45, 6, 88],
+                    'following': [43, 65, 85, 123, 2]},
+            '2': {
+                    'followers': [1, 3, 4, 7, 88],
+                    'following': [1, 343, 65, 85, 123]},
+            '45': {
+                    'followers': [3, 6, 88],
+                    'following': [43, 65, 1, 123]}
+            }
+
+unique_nodes = [1, 2, 3, 4, 6, 7, 43, 45, 65, 85, 88, 123, 343]
+
+edges = [(2, 1), (3, 1), (45, 1), (6, 1), (88, 1),
+         (1, 43), (1, 65), (1, 85), (1, 123), (1,2),
+         (1, 2), (3, 2), (4, 2), (7, 2), (88, 2),
+         (2, 1), (2, 343), (2, 65), (2, 85), (2, 123),
+         (3, 45), (6, 45), (88, 45),
+         (45, 43), (45, 65), (45, 1), (45, 123)]
+
+
+def test_unique_nodes():
+    nodes = un_nodes_dict(test_dict)
+    nodes.sort()
+    assert nodes == unique_nodes
+
+
+def test_edges():
+    global test_dict
+    all_edges = edges_dict(test_dict)
+    assert all_edges == edges
