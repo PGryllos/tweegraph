2fe5e48c010b414ed46f73bd8b1eb072c21dda2e
prvl <gryllosprokopis@gmail.com>
2016-05-15 03:44:31 +0300

    Add simple setup.py
    mainly for not having to keep all scripts in the folder with package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+
+setup(name='tweegraph',
+      version='1.0',
+      description='A package for crawling twitter',
+      author='Prokopios Gryllos',
+      author_email='gryllosprokopis@gmail.com',
+      url='https://github.com/PGryllos/tweegraph',
+      packages=['tweegraph'],
+     )
